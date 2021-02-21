ee3d73001def59b27f3eb84d33418a7e316def6d
MrYsLab <MisterYsLab@gmail.com>
2021-02-21 16:47:20 -0500

    Digital Input

---
+++ README.md
@@ -0,0 +1,5 @@
+# Telemetrix-RPi-Pico
+
+This is an early version of a Telemetrix client for the Raspberry Pi Pico.
+
+This is a work in progress.
---
+++ setup.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+
+from setuptools import setup
+
+with open('README.md') as f:
+    long_description = f.read()
+
+
+setup(
+    name='telemetrix-pi-pico',
+    packages=['telemetrix_rpi_pico'],
+    install_requires=['pyserial'],
+
+    version='0.1',
+    description="Remotely Control And Monitor A Raspberry Pi Pico",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+
+    author='Alan Yorinks',
+    author_email='MisterYsLab@gmail.com',
+    url='https://github.com/MrYsLab/telemetrix-rpi-pico',
+    download_url='https://github.com/MrYsLab/telemetrix-rpi-pico',
+    keywords=['telemetrix', 'Raspberry_Pi_Pico', 'Protocol', 'Python'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Other Environment',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Education',
+        'License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 39',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
+)
+
