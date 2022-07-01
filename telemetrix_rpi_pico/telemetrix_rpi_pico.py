57d6ea7cd5521ed0cda7dda7606ec79bb117120c
MrYsLab <MisterYsLab@gmail.com>
2022-07-01 19:11:10 -0400

    Address issue #9

--- telemetrix_rpi_pico/telemetrix_rpi_pico.py
+++ telemetrix_rpi_pico/telemetrix_rpi_pico.py
@@ -691,7 +691,17 @@ class TelemetrixRpiPico(threading.Thread):
         """
         Set a pin as an analog input.
 
-        :param adc_number: ADC Number 0-3 - ADC 3 is the temp sensor
+        :param adc_number: ADC Number 0-4
+
+                           ADC numbers are mapped as following:
+                           ADC0 = GPIO 26 (Physical Pin 31)
+                           ADC1 = GPIO 27 (Physical Pin 32)
+                           ADC2 = GPIO 28 (Physical Pin 34)
+
+                           Internal Mapping
+                           ADC3 = GPIO 29 (Physical Pin 35) ADC Reference Voltage
+                           ADC4 = GPIO 30 (No Physical pin - mapped internally)
+                                          CPU temperature
 
         :param differential: difference in previous to current value before
                              report will be generated
